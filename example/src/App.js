35974bc2bfd05d2867d85a6a65fad1565ff8b9d2
Брагина Галина Васильевна <GBragina@vimpelcom.ru>
2019-10-29 13:05:14 +0300

    init create-react-library@2.6.7

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import ExampleComponent from 'testlib'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <ExampleComponent text='Modern React component module' />
+      </div>
+    )
+  }
+}
